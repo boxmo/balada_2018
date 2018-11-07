40e8d6bad5c566d1e2cd10586517d78764ef6b5a
boxmo <mztbrum@gmail.com>
2018-11-07 01:36:04 -0200

    deploy

--- balada_2018.e31bb0bc.js
+++ balada_2018.e31bb0bc.js
@@ -17432,7 +17432,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45337" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36333" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
