b7f414ef1b01ceef1e31158f08b37eb9771d1f53
boxmo <mztbrum@gmail.com>
2018-11-13 04:14:46 -0200

    deploy

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36247" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38875" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
